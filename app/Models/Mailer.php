13dac1564f18eb12071031fd95eb6768b79af3c5
Igor Lisboa <igor.rodrigues.lisboa@gmail.com>
2021-01-27 23:39:03 -0300

    Fazendo model de mailers e members

---
+++ app/Models/Mailer.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Models;
+
+use App\Traits\Uuid;
+use Illuminate\Database\Eloquent\Model;
+
+class Mailer extends Model
+{
+    use Uuid;
+
+    /**
+     * The primary key associated with the table.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'uid';
+
+    /**
+     * The data type of the auto-incrementing ID.
+     *
+     * @var string
+     */
+    protected $keyType = 'string';
+
+    /**
+     * Indicates if the model's ID is auto-incrementing.
+     *
+     * @var bool
+     */
+    public $incrementing = false;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'user_uid',
+        'slug',
+        'priority',
+        'quota_qtd',
+        'quota_qtd_max',
+        'quota_renovation',
+        'active'
+    ];
+
+    /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var bool
+     */
+    public $timestamps = false;
+}
